0b93f8079d69222b3fb6cc27aa054ee90dedfaf4
Allen Li <allen@allen.li>
2015-01-28 00:35:27 -0800

    oski bear sample

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var elasticsearch = require('elasticsearch');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -69,22 +70,45 @@ var server = stuff.server;
 var io = stuff.io;
 io.on('connection', function (socket) {
     socket.on('query', function (data, callback) {
-        callback(lookup(data.query));
+        lookup(data.query, callback);
     })
 });
 
-// function for lookup up the results of a query.
-function lookup(query) {
-    return [
-    {
-        name: query,
-        phone: '510-555-1234',
-    }
-    ,
-    {
-        name: 'Allen Li',
-        phone: '510-555-1234',
-    }
-
-    ];
+function createIndexIfNonexistant() {
+    esClient.indices.exists({index: 'directory'}).then(
+        function (exists) {
+            if (!exists) {
+                esClient.indices.create({index: 'directory'})
+                esClient.index({
+                    index: 'directory',
+                    type: 'person',
+                    body: {
+                        name: 'Oski Bear',
+                        phone: '510-555-1234'
+                    }
+                })
+            }
+        });
 }
+
+// conveninence housekeeping function
+function destroyIndex() {
+    esClient.indices.delete({index: 'directory'})
+}
+
+esHost = process.env.BONSAI_URL || 'localhost:9200'
+var esClient = new elasticsearch.Client({
+    host: esHost
+})
+createIndexIfNonexistant();
+
+// function for lookup up the results of a query.
+function lookup(query, callback) {
+    esClient.search({index: 'directory', q: query},
+        function (error, response) {
+            var hits = response.hits.hits;
+            var processedHits = hits.map(function (x) { return x._source });
+            callback(processedHits);
+        });
+}
+
