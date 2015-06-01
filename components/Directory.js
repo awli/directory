7a7a0bb4f2fd85d50a2821078baa7576409266b2
Allen Li <allen@allen.li>
2015-06-01 16:15:33 -0700

    split up components

--- components/Directory.js
+++ components/Directory.js
@@ -1,7 +1,15 @@
 var React = require('react');
+var QueryBar = require('./QueryBar');
+var Results = require('./Results');
 
 module.exports = React.createClass({
   render: function () {
-    return <p> Hello Directory! </p>
+    return (
+      <div className="container-fluid">
+        <h1>Directory Lookup</h1>
+        <QueryBar />
+        <Results {...this.props}/>
+      </div>
+    )
   }
 });
---
+++ components/Results.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function () {
+    return (
+      <ul>
+        <li> Result 1 </li>
+        <li> Result 2 </li>
+      </ul>
+    )
+  }
+});
