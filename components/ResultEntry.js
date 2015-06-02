b435293370485c0969819b34a3341c49dbedacbd
Allen Li <allen@allen.li>
2015-06-01 18:53:40 -0700

    implement phone linking

--- components/ResultEntry.js
+++ components/ResultEntry.js
@@ -1,11 +1,18 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  phoneLink: function () {
+    return 'tel://' + this.props.result.phone;
+  },
   render: function () {
     return (
       <tr>
         <td> {this.props.result.name} </td>
-        <td> {this.props.result.phone} </td>
+        <td>
+          <a href={this.phoneLink()}>
+            {this.props.result.phone}
+          </a>
+        </td>
       </tr>
     )
   }
